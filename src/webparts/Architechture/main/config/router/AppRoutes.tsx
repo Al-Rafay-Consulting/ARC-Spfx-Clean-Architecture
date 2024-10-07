7183973a3e3c0ded768c6e010a5cd2121b26f027
kabir kaka <kabir.kaka@alrafayconsulting.com>
2024-10-07 20:17:47 +0500

    redux-configuration and sample todo app implementation

--- src/webparts/Architechture/main/config/router/AppRoutes.tsx
+++ src/webparts/Architechture/main/config/router/AppRoutes.tsx
@@ -3,6 +3,7 @@ import { HashRouter, Route, Routes } from 'react-router-dom';
 import NavBar from '../../shared/components/Navbar/Navbar';
 import CustomerManagement from '../../features/module-01';
 import CustomerManagement2 from '../../features/module-02';
+import Todos from '../../features/module-01/components/container/Todos';
 
 function AppRoutes() {
 
@@ -10,7 +11,7 @@ function AppRoutes() {
         <HashRouter>
             <NavBar />
                 <Routes>
-                    <Route path="/" element={<CustomerManagement />} />
+                    <Route path="/" element={<Todos />} />
                     <Route path="/module-01/*" element={<CustomerManagement />} />
                     <Route path="/module-02/*" element={<CustomerManagement2 />} />
                 </Routes>
--- src/webparts/Architechture/main/features/module-01/index.tsx
+++ src/webparts/Architechture/main/features/module-01/index.tsx
@@ -1,16 +1,15 @@
 import * as React from 'react';
 import CustomerCreate from './components/container/CustomerCreate';
 import { Route, Routes } from 'react-router-dom';
-import CustomerListing from './components/container/CustomerListing';
+import Todos from './components/container/Todos';
 
 export default class CustomerManagement extends React.Component {
     public render() {
-
         return (
             <Routes>
                 <Route path="/" element={<CustomerCreate />} />
                 <Route path="/create" element={<CustomerCreate />} />
-                <Route path="/list" element={<CustomerListing />} />
+                <Route path="/list" element={<Todos />} />
             </Routes>
         );
     }
