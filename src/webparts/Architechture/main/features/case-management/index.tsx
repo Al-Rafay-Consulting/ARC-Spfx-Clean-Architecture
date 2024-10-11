96ee8559d5660187f1938491c5f65afcfa124c64
kabir kaka <kabir.kaka@alrafayconsulting.com>
2024-10-11 11:55:05 +0500

    table, filter drawer shared components

--- src/webparts/Architechture/main/features/case-management/index.tsx
+++ src/webparts/Architechture/main/features/case-management/index.tsx
@@ -1,11 +1,13 @@
 import * as React from "react";
 import { Route, Routes } from "react-router-dom";
 import CasesListing from "./components/container/CasesListing";
+import CaseManagementLanding from "./components/container/CaseManagementLanding";
 
 export default class CaseManagement extends React.Component {
   public render() {
     return (
       <Routes>
+        <Route path="/" element={<CaseManagementLanding />} />
         <Route path="/list" element={<CasesListing />} />
       </Routes>
     );
